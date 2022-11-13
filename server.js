13d9770f316b126f51b4380b93af3ac4b7e4d578
Angelica Camacho <113385869+Camacho-A@users.noreply.github.com>
2022-11-13 16:08:21 -0500

    Show.ejs rendered

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const methodOverride = require("method-override")
+const budgets = require("./models/budget")
 const budget = require("./models/budget")
 
 const PORT = process.env.PORT || 3000 
@@ -9,30 +10,40 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(express.urlencoded({extended: true}))
+app.use(morgan("tiny"))
+app.use(methodOverride("_method"))
+app.use("/static", express.static("public"))
+app.use('/public', express.static('public'));
+
 
 //  Index Routes
 app.get("/budget", (req, res) => {
-    res.render("index.ejs",
-        {
-            allBudget: budget
-        })
+    res.render("index.ejs", {
+        allBudgets: budget
+    })
+       
 })
 
-// // Show Route
-// app.get("/budget/:index", (req, res) => {
-//     res.render("show.ejs")
-
-// })
-
-// // New Route
-// app.get("/budget/new", (req, res) => {
-//     res.render("new.ejs")
-// })
+// New Route
+app.get("/budget/new", (req, res) => {
+    res.render("new.ejs")
+})
 
 // //  Create Route
 // app.post("/budget", (req, res) => {
 
 // })
+
+// Show Route
+app.get("/budget/:id", (req, res) => {
+    res.render("show.ejs", {
+        budgets: budget[req.params.id]
+        
+        
+    })
+
+})
 
 
 app.listen(PORT, () => {
