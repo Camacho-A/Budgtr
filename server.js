4fdcea0f6c6acfc7f6a8c8999405dc5902e8afa2
Angelica Camacho <113385869+Camacho-A@users.noreply.github.com>
2022-11-13 13:17:28 -0500

    Rendering Index.ejs

---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const budget = require("./models/budget")
+
+
+const app = express()
+
+
+//  Index Routes
+app.get("/budget", (req, res) => {
+    res.render("index.ejs")
+})
+
+// // Show Route
+// app.get("/budget/:index", (req, res) => {
+//     res.render("show.ejs")
+
+// })
+
+// // New Route
+// app.get("/budget/new", (req, res) => {
+//     res.render("new.ejs")
+// })
+
+// //  Create Route
+// app.post("/budget", (req, res) => {
+
+// })
+
+const PORT = process.env.PORT || 3000 
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`)
+})
