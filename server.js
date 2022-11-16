70916ef32fe715910a3219d9cc18e1fb457c4e01
Angelica Camacho <113385869+Camacho-A@users.noreply.github.com>
2022-11-15 21:30:49 -0500

    color function added to total

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const methodOverride = require("method-override")
-const budgets = require("./models/budget")
+
 const budget = require("./models/budget")
 
 const PORT = process.env.PORT || 3000 
@@ -10,19 +10,22 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(express.urlencoded({extended: true}))
+
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan("tiny"))
 app.use(methodOverride("_method"))
 app.use("/static", express.static("public"))
 app.use('/public', express.static('public'));
 
 
-//  Index Routes
+//  Home page
+app.get("/", (req, res) => res.redirect("/budget"))
+
+// Index route and Returns all budgets
 app.get("/budget", (req, res) => {
     res.render("index.ejs", {
         allBudgets: budget
-    })
-       
+    })   
 })
 
 // New Route
@@ -30,17 +33,18 @@ app.get("/budget/new", (req, res) => {
     res.render("new.ejs")
 })
 
-// //  Create Route
-// app.post("/budget", (req, res) => {
-
-// })
+//  Create Route
+app.post("/budget", (req, res) => {
+    budget.push(req.body)
+    res.redirect("/budget")
+    
+})
 
 // Show Route
 app.get("/budget/:id", (req, res) => {
     res.render("show.ejs", {
         budgets: budget[req.params.id]
-        
-        
+         
     })
 
 })
